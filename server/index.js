bee42145fda6396e0bd7f0f3cc0da1b8a2988490
Valeria <lerik13@gmail.com>
2022-01-21 00:33:02 -0700

    created structure of app, connect to DB

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const config =  require("config")
+
+const app = express()
+const PORT = config.get("serverPort")
+
+const start = async () => {
+	try {
+		await mongoose.connect(config.get("dbUrl"))
+
+		app.listen(PORT, () => {
+			console.log('Server started on port ', PORT)
+		})
+	} catch(e) {
+
+	}
+}
+
+start();
---
+++ server/models/User.js
@@ -0,0 +1,12 @@
+const {Schema, model} = require("mongoose")
+
+const User = new Schema({
+	email: {type: String, required: true, unique: true},
+	password: {type: String, required: true},
+	diskSpace: {type: Number, default: 1024**3*10},
+	usedSpace: {type: Number, default: 0},
+	avatar: {type: String},
+	files: [{type: ObjectId, ref: 'File'}]
+})
+
+module.exports = model('User', User)
