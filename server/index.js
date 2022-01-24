0117b0e2ce500064b68f17d21c9942ca77b82bda
Valeria <lerik13@gmail.com>
2022-01-23 22:35:44 -0700

    created Router '/api/auth/registration'

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const config =  require("config")
+const authRouter = require("./routes/auth.routes")
 
 const app = express()
-const PORT = config.get("serverPort")
+const PORT = config.get('serverPort')
+
+app.use(express.json())
+app.use("/api/auth", authRouter)
+
 
 const start = async () => {
 	try {
@@ -13,7 +18,7 @@ const start = async () => {
 			console.log('Server started on port ', PORT)
 		})
 	} catch(e) {
-
+		console.log(e)
 	}
 }
 
--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,4 @@
-const {Schema, model} = require("mongoose")
+const {Schema, model, ObjectId} = require("mongoose")
 
 const User = new Schema({
 	email: {type: String, required: true, unique: true},
@@ -9,4 +9,4 @@ const User = new Schema({
 	files: [{type: ObjectId, ref: 'File'}]
 })
 
-module.exports = model('User', User)
+module.exports = model("User", User)
