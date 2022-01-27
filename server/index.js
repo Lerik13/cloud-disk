450b4fffd6e30384b50dd7ed9f788e6e29b6e2c6
Valeria <lerik13@gmail.com>
2022-01-26 19:50:23 -0700

    fixed problem with CORS, using middleware

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const authRouter = require("./routes/auth.routes")
 
 const app = express()
 const PORT = config.get('serverPort')
+const corsMiddleware = require('./middleware/cors.middleware')
 
+app.use(corsMiddleware)
 app.use(express.json())
 app.use("/api/auth", authRouter)
 
