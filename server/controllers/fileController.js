6550208c48fd17acf49d810d96cf711073f05ab3
Valeria <lerik13@gmail.com>
2022-02-04 11:05:19 -0700

    Upload files - corrected .gitignore

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -1,4 +1,6 @@
 const fileService = require('../services/fileService')
+const config = require('config')
+const fs = require('fs') // work with FileSystem
 const User = require('../models/User')
 const File = require('../models/File')
 
@@ -28,10 +30,58 @@ class FileController {
 	async getFiles(req, res) {
 		try {
 			const files = await File.find({user: req.user.id, parent: req.query.parent})
-			return res.json({files})
+			return res.json(files)
 		} catch (e) {
 			console.log(e);
 			return res.status(500).json({message: "Cannot get files"})
+		}
+	}
+
+	async uploadFile(req, res) {
+		try {
+			const file = req.files.file
+
+			const parent = await File.findOne({user: req.user.id, _id: req.body.parent})
+			const user = await User.findOne({_id: req.user.id})
+
+			if (user.usedSpace + file.size > user.diskSpace) {
+				return res.status(400).json({message: "There are no space on the disk"})
+			}
+
+			user.usedSpace = user.usedSpace + file.size
+
+			let path;
+			if (parent) {
+				path = `${config.get('filePath')}\\${user._id}\\${parent.path}\\${file.name}`
+			} else {
+				path = `${config.get('filePath')}\\${user._id}\\${file.name}`
+			}
+
+			if (fs.existsSync(path)) {
+				return res.status(400).json({message: "File already exist"})
+			}
+
+			await file.mv(path)  //Move file to path (express-fileupload)
+
+			const type = file.name.split('.').pop()
+
+			const dbFile = new File({
+				name: file.name,
+				type,
+				size: file.size,
+				path: parent?.path,
+				parent: parent?._id,
+				user: user._id
+			})
+
+			await dbFile.save()
+			await user.save()
+
+			res.json(dbFile)
+
+		} catch(e) {
+			console.log(e);
+			return res.status(500).json({message: "Upload error"})
 		}
 	}
 }
--- server/routes/file.routes.js
+++ server/routes/file.routes.js
@@ -5,6 +5,7 @@ const fileController = require('../controllers/fileController')
 const router = new Router()
 
 router.post('', authMiddleware, fileController.createDir)
+router.post('/upload', authMiddleware, fileController.uploadFile)
 router.get('', authMiddleware, fileController.getFiles)
 
 module.exports = router
