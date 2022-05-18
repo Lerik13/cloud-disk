9da1f7b2b9cba5ab2929d3650fe93640e55cc2e6
Valeria <lerik13@gmail.com>
2022-05-18 12:06:41 -0600

    fixed error with API_URL + preparation to deploy

---
+++ front_end/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+
+const PORT = process.env.PORT || 8080
+
+const app = express()
+
+app.use(express.static(__dirname))
+app.use(express.static(path.resolve(__dirname, 'build')))
+
+app.use(express.static(path.join(__dirname, 'static')))
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+app.listen(PORT)
--- front_end/src/actions/file.js
+++ front_end/src/actions/file.js
@@ -67,7 +67,7 @@ export function uploadFile(file, dirId) {
 			dispatch(showUploader())
 			dispatch(addUploadFile(uploadFile))
 
-			const response = await axios.post(`http://localhost:5000/api/files/upload`, formData, {
+			const response = await axios.post(`${API_URL}api/files/upload`, formData, {
 				headers: {Authorization: `Bearer ${localStorage.getItem('token')}`},
 				onUploadProgress: progressEvent => { // get size of file, using Header
 					const totalLength = progressEvent.lengthComputable ? progressEvent.total : progressEvent.target.getResponseHeader('content-length') || progressEvent.target.getResponseHeader('x-decompressed-content-length');
@@ -86,7 +86,7 @@ export function uploadFile(file, dirId) {
 
 export async function downloadFile(file) {
 	//console.log('downloadFile =', file)
-	const response = await fetch(`http://localhost:5000/api/files/download?id=${file._id}`, {
+	const response = await fetch(`${API_URL}api/files/download?id=${file._id}`, {
 		headers: {
 			Authorization: `Bearer ${localStorage.getItem('token')}`
 		}
@@ -107,7 +107,7 @@ export async function downloadFile(file) {
 export function deleteFile(file) {
 	return async dispatch => {
 		try {
-			const response = await axios.delete(`http://localhost:5000/api/files?id=${file._id}`, {
+			const response = await axios.delete(`${API_URL}api/files?id=${file._id}`, {
 				headers: {
 					Authorization: `Bearer ${localStorage.getItem('token')}`
 				}
@@ -125,7 +125,7 @@ export function deleteFile(file) {
 export function searchFiles(search) {
 	return async dispatch => {
 		try {
-			const response = await axios.get(`http://localhost:5000/api/files/search?search=${search}`, {
+			const response = await axios.get(`${API_URL}api/files/search?search=${search}`, {
 				headers: {
 					Authorization: `Bearer ${localStorage.getItem('token')}`
 				}
