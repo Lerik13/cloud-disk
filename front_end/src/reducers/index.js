11f36b2085d58681f62caf547b3cbece7a71eeda
Valeria <lerik13@gmail.com>
2023-01-26 13:14:26 -0700

    change to configStore from @reduxjs/toolkit

--- front_end/src/reducers/index.js
+++ front_end/src/reducers/index.js
@@ -1,16 +1,14 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
-import {composeWithDevTools} from "redux-devtools-extension";
-import thunk from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 import fileReducer from "./fileReducer";
 import userReducer from "./userReducer";
 import uploadReducer from "./uploadReducer";
 import appReducer from "./appReducer";
 
-const rootReducer = combineReducers({
-	user: userReducer,
-	files: fileReducer,
-	upload: uploadReducer,
-	app: appReducer
+export const store = configureStore({
+	reducer: {
+		user: userReducer,
+		files: fileReducer,
+		upload: uploadReducer,
+		app: appReducer
+	}
 })
-
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
