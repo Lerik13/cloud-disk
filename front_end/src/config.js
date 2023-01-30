2b02f5fe590609abd9345299868c4eee1d2b9d34
Valeria <lerik13@gmail.com>
2023-01-29 18:36:10 -0700

    changed config

--- front_end/src/config.js
+++ front_end/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = `http://localhost:5000/`;
-//export const API_URL = `https://cloud-disk-app.herokuapp.com/`;
+//export const API_URL = `http://localhost:5000/`;
+export const API_URL = `https://cloud-disk.onrender.com/`;
